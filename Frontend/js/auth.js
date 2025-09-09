6456560fd70274729f1365b291d77fb2c71f55ff
LaureLavie <laure.lavie@gmail.com>
2025-09-09 16:20:32 +0200

    hotestyle+btn logout

--- Frontend/js/auth.js
+++ Frontend/js/auth.js
@@ -19,7 +19,7 @@ if (loginForm) {
     console.log("Email:", email, "Password:", password);
 
     try {
-      const res = await fetch(`${API_URL}/api/auth/login`, {
+      const res = await fetch("http://localhost:3000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
