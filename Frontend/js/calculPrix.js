c70a6414eb94278eb834e0c892de448f799fb8b7
LaureLavie <laure.laviegmail.com>
2025-10-08 15:04:57 +0200

    options menage

--- Frontend/js/calculPrix.js
+++ Frontend/js/calculPrix.js
@@ -1,4 +1,9 @@
-export function calculPrixReservation(nombrePersonnes, nuits, personnesSupplementaires = 0, options = {}) {
+export function calculPrixReservation(
+  nombrePersonnes,
+  nuits,
+  personnesSupplementaires = 0,
+  options = {}
+) {
   const tarifs = [65, 75, 85, 95, 105, 115];
   let prixTotal = tarifs[nombrePersonnes - 1] * nuits;
   if (options.menage) prixTotal += 30;
